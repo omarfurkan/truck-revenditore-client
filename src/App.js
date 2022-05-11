45de4b56a38f5bc9196c0a893b37d8560017ba39
omarfurkan <omarfurkan92@gmail.com>
2022-05-11 06:18:12 +0600

    make some component and navbar by installing reactbootsrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home'
 
 function App() {
   return (
-    <div className="App">
-
+    <div >
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar fixed='top' bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">Truck Revenditore</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
