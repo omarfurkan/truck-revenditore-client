c6ecf35020627cbd40394331b2556b99458e97da
omarfurkan <omarfurkan92@gmail.com>
2022-05-14 01:21:28 +0600

    auth is done in login and register page/

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
 import loginImg from '../../Images/login.png'
 import SocialLogin from '../SocialLogin/SocialLogin';
 import './Login.css'
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const navigate = useNavigate();
+    const [signInWithEmailAndPassword, user, error] = useSignInWithEmailAndPassword(auth);
+
+
+
+    if (user) {
+        navigate('/')
+    }
+
+
+
+
+
+    const handleEmailChange = event => {
+        setEmail(event.target.value)
+    }
+    const handlePasswordChange = event => {
+        setPassword(event.target.value)
+    }
+    const handleUserSignIn = event => {
+        event.preventDefault();
+        signInWithEmailAndPassword(email, password)
+    }
+
+
+
     return (
         <div className='container register-container my-5'>
 
@@ -14,15 +44,16 @@ const Login = () => {
 
             <div>
                 <h3 className='text-center'>LogIn</h3>
-                <form>
+                <form onSubmit={handleUserSignIn}>
                     <label htmlFor="email">Email</label>
                     <br />
-                    <input type="email" name="email" required />
+                    <input onChange={handleEmailChange} type="email" name="email" required />
                     <br />
                     <label htmlFor="password">Password</label>
                     <br />
-                    <input type="password" name="password" required />
+                    <input onChange={handlePasswordChange} type="password" name="password" required />
                     <br />
+                    <p className='text-danger'>{error?.message}</p>
                     <button className="update-btn" type="submit">LogIn</button>
                 </form>
                 <p className='mt-3 regis-log text-center' >New at Truck Revenditore?
